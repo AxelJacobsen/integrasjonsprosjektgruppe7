ff8911fd1d0fc09917ce564e634cb8304a97c415
Lars LR <lars.l.r@hotmail.com>
2022-09-21 15:32:05 +0200

    [#10] Fikset feil med SlippysToCoords-funksjonen. closes #10

--- backend/mapboxexamples/mapboxexamples.go
+++ backend/mapboxexamples/mapboxexamples.go
@@ -13,13 +13,29 @@ import (
  *	@param xtile - X-coordinate for slippys map.
  *	@param ytile - Y-coordinate for slippys map.
  *
+ *	@see https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_numbers_to_lon./lat.
+ *
  *	@return longitude, latitude
  */
 func SlippysToCoords(zoom float64, xtile float64, ytile float64) (float64, float64) {
+	// Convert
 	n := math.Pow(2, zoom)
 	longitude := xtile/n*360.0 - 180.0
 	latitude := math.Atan(math.Sinh(math.Pi*(1.0-2.0*ytile/n))) * 180.0 / math.Pi
 
+	// Restrict to mapbox's boundries
+	if longitude > 180 {
+		longitude = 180
+	} else if longitude < -180 {
+		longitude = -180
+	}
+	if latitude > 85.0511 {
+		latitude = 85.0511
+	} else if latitude < -85.0511 {
+		latitude = -85.0511
+	}
+
+	// Return
 	return longitude, latitude
 }
 
@@ -30,6 +46,8 @@ func SlippysToCoords(zoom float64, xtile float64, ytile float64) (float64, float
  *	@param longitude - The longitude.
  *	@param latitude - The latitude.
  *
+ *	@see https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon./lat._to_tile_numbers_2
+ *
  *	@return xtile, ytile
  */
 func CoordsToSlippys(zoom float64, longitude float64, latitude float64) (float64, float64) {
@@ -37,7 +55,6 @@ func CoordsToSlippys(zoom float64, longitude float64, latitude float64) (float64
 	n := math.Pow(2, zoom)
 	xtile := n * ((longitude + 180.0) / 360.0)
 	ytile := n * (1 - (math.Log(math.Tan(latitude_rad)+1.0/math.Cos(latitude_rad)) / math.Pi)) / 2.0
-
 	return xtile, ytile
 }
 
@@ -85,12 +102,11 @@ func GetStaticmapURL(zoom float64, longitude float64, latitude float64) string {
 	return fmt.Sprintf(
 		//`https://api.mapbox.com/styles/v1/mapbox/%s/static/pin-s-l+000(-87.0186,32.4055)/-87.0186,32.4055,14/500x300?access_token=%s`, Example with streets etc
 		//`https://api.mapbox.com/styles/v1/mapbox/%s/static/-77.043686,38.892035,15.25,0,60/400x400?access_token=%s"`, Doesnt work!
-		`https://api.mapbox.com/styles/v1/mapbox/%s/static/[%f,%f,%f,%f]/400x400?access_token=%s`,
+		`https://api.mapbox.com/styles/v1/mapbox/%s/static/%f,%f,%f/512x512?access_token=%s`,
 		"streets-v11",
 		longitude,
 		latitude,
-		longitude+45.0/zoom, // A little arbitrary...
-		latitude+45.0/zoom,  // ...Should sort this out later
+		zoom,
 		testproject.MAPTOKEN,
 	)
 }
